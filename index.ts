9260b9fd20fd383856dc37d655d0b519c7d04836
ellia passinge <ellia.passinge@eliberty.fr>
2025-08-13 13:09:58 +0200

    fix: typing

--- index.ts
+++ index.ts
@@ -3,67 +3,79 @@ import { sendButtons } from "./integrations/primary";
 import { closeTicket, createTicket } from "./helper/ticket";
 import { config } from "./config";
 
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
 
-client.once('ready', async () => {
+client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
   const channel = client.channels.cache.get(config.channels.defaultTchatting);
   if (channel) {
-    console.log('Bonjour !');
+    console.log("Bonjour !");
   } else {
-    console.error('Channel not found!');
+    console.error("Channel not found!");
   }
 
-  await sendButtons(client); 
+  await sendButtons(client);
 });
 
-client.on('interactionCreate', async (interaction: Interaction) => {
+client.on("interactionCreate", async (interaction: Interaction) => {
   if (interaction.isChatInputCommand()) {
     const command = client.commands.get(interaction.commandName);
-    if (!command){
+    if (!command) {
       return;
     }
 
     try {
       await command.execute(interaction);
     } catch (error) {
-      console.error(`Erreur during command's execution ${interaction.commandName} :`, error);
+      console.error(
+        `Erreur during command's execution ${interaction.commandName} :`,
+        error,
+      );
       if (!interaction.replied) {
-        await interaction.reply({ content: "❌ Une erreur est survenue.", flags: MessageFlags.Ephemeral });
+        await interaction.reply({
+          content: "❌ Une erreur est survenue.",
+          flags: MessageFlags.Ephemeral,
+        });
       }
     }
     return;
   }
 
-
-  if (!interaction.isButton()){
+  if (!interaction.isButton()) {
     return;
   }
 
-  switch(interaction.customId) {
-    case 'close':
+  switch (interaction.customId) {
+    case "close":
       await closeTicket(interaction);
       break;
-    case 'complaint': 
-      await createTicket('plainte-', interaction);
+    case "complaint":
+      await createTicket("plainte-", interaction);
       break;
-    case 'new-membre': 
-      await createTicket('membre-', interaction);
+    case "new-membre":
+      await createTicket("membre-", interaction);
       break;
-    case 'other': 
-      await createTicket('autre-', interaction);
+    case "other":
+      await createTicket("autre-", interaction);
       break;
-    case 'suggest':
-      await createTicket('suggestion-', interaction);
+    case "suggest":
+      await createTicket("suggestion-", interaction);
       break;
     default:
-      return await interaction.reply({ content: `Bouton ${interaction.customId} non pris en compte actuellement. Merci de contacter 𝒟𝑒𝓋𝒾𝓁♡𝒜𝓃𝑔𝑒𝓁𝒾𝓀𝒶`, flags: MessageFlags.Ephemeral });
+      return await interaction.reply({
+        content: `Bouton ${interaction.customId} non pris en compte actuellement. Merci de contacter 𝒟𝑒𝓋𝒾𝓁♡𝒜𝓃𝑔𝑒𝓁𝒾𝓀𝒶`,
+        flags: MessageFlags.Ephemeral,
+      });
   }
-})
+});
 
 client.login(process.env.DISCORD_TOKEN);
